d1977e7b645f25b2e6c9c6b01c28bba8875899bb
JMartin <jmartin@umag.cl>
2024-03-19 17:18:27 -0300

    c

--- app/db/models.py
+++ app/db/models.py
@@ -13,6 +13,7 @@ from app.db.models import Base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 class User(Base):
+    
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nombre = Column(String)
